649f88bfcd2c71ed1886196a20bc0dc370af1a70
Marc Jou <mjou@userzoom.com>
2021-11-24 15:24:49 +0100

    Updated deps. Created GHA's test.yml

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject nomnom/bunnicula "2.2.2"
+(defproject nomnom/bunnicula "2.2.3-SNAPSHOT-1"
   :description "Bunnicula: RabbitMQ client"
   :url "https://github.com/nomnom-insights/nomnom.bunnicula"
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/tools.logging "1.1.0"]
-                 [com.rabbitmq/amqp-client "5.11.0"]
+                 [com.rabbitmq/amqp-client "5.14.0"]
                  [com.stuartsierra/component "1.0.0"]
-                 [cheshire "5.10.0"]]
+                 [cheshire "5.10.1"]]
   :deploy-repositories {"clojars" {:sign-releases false}}
   :min-lein-version "2.5.0"
   :global-vars {*warn-on-reflection* true}
@@ -14,4 +14,4 @@
             :year 2018
             :key "mit"}
   :profiles {:dev
-             {:dependencies  [[ch.qos.logback/logback-classic "1.2.3"]]}})
+             {:dependencies  [[ch.qos.logback/logback-classic "1.2.7"]]}})
